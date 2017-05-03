c3f2fc250b79ad17682c810474dfe349ac55cd6b
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-05-03 13:40:47 +0200

    Start using attachments to return formatted movie info.

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 module.exports = function(bp) {
+  const imdb = require('imdb-api');
+
   bp.middlewares.load();
 
-  bp.hear({platform: 'slack', type: 'message', direct: true})
-    .then((event, next) => {
-      bp.slack.sendText(event.channel.id, 'Hi, there!');
-      if (next) next();
+  const reFind = /^\s*find /i;
+  bp.hear({platform: 'slack', type: 'message', direct: true, text: reFind}, (event, next) => {
+    let query = event.text.replace(reFind, '');
+    if (!query) next();
+
+    imdb.get(query)
+      .then((result) => {
+        if (result) {
+          bp.slack.sendAttachments(event.channel.id, [
+            {
+              title: result.title,
+              fields: [
+                {
+                  title: 'Rated',
+                  value: result.rated,
+                  short: 'true'
+                },
+                {
+                  title: 'Rating',
+                  value: result.rating,
+                  short: true
+                },
+                {
+                  title: 'Genres',
+                  value: result.genres
+                },
+                {
+                  title: 'Plot',
+                  value: result.plot
+                }
+              ]
+            }
+          ]);
+        }
+      })
+    .catch(() => {
+      bp.slack.sendText(event.channel.id, 'I couldn\'t find a movie like that. Sorry.');
     });
+  });
 };
