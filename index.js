dc574266d1f5c77ef6998ad0c1dc5c8b50509a3f
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-05-03 11:35:48 +0200

    Add initial direct message interaction.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = function(bp) {
   bp.middlewares.load();
+
+  bp.hear({platform: 'slack', type: 'message', direct: true})
+    .then((event, next) => {
+      bp.slack.sendText(event.channel.id, 'Hi, there!');
+      if (next) next();
+    });
 };
