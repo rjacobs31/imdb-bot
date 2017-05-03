dc5ac49c9e28c82621a10ce15ea1e4e5bb54cfb1
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-05-03 17:10:46 +0200

    Implement basic similar search function.

--- utils/similar_search.js
+++ utils/similar_search.js
@@ -1,1 +1,37 @@
 let cheerio = require('cheerio');
+let imdb = require('imdb-api');
+let rp = require('request-promise');
+let Promise = require('bluebird');
+
+const urlPrefix = 'https://www.imdb.com/title/';
+const imdbIdRe = /^tt[0-9]+$/;
+
+function getSimilar(id) {
+  let url = id;
+  if (!id) return Promise.reject(new Error('ID not set'));
+  if (imdbIdRe.test(id)) {
+    url = urlPrefix + id;
+  }
+
+  let options = {
+    transform: function(body) {
+      return cheerio.load(body);
+    }
+  };
+  return rp(url, options)
+    .then(($) => {
+      let similarNodes = $('#titleRecs .rec_page .rec_item[data-tconst]');
+      let ids = [];
+      similarNodes.each((i, el) => {
+        ids[i] = $(el).attr('data-tconst');
+      });
+
+      return Promise.map(ids, (simId) => {
+        return imdb.getById(simId);
+      });
+    });
+}
+
+module.exports = {
+  getSimilar: getSimilar
+};
