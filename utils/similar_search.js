79d83e9d89b920ef80f9c082634dbbeedb54a9dd
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2017-05-07 18:39:21 +0200

    Route similarity search through cache.

--- utils/similar_search.js
+++ utils/similar_search.js
@@ -1,3 +1,4 @@
+let _ = require('lodash');
 let cheerio = require('cheerio');
 let rp = require('request-promise');
 let Promise = require('bluebird');
@@ -7,12 +8,14 @@ let cachedMovie = require('./cached_movie');
 const urlPrefix = 'https://www.imdb.com/title/';
 const imdbIdRe = /^tt[0-9]+$/;
 
-function getSimilar(id) {
+function getSimilar(knex, id, count) {
   let url = id;
   if (!id) return Promise.reject(new Error('ID not set'));
   if (imdbIdRe.test(id)) {
     url = urlPrefix + id;
   }
+
+  if (!count) count = 10;
 
   let options = {
     transform: function(body) {
@@ -22,14 +25,8 @@ function getSimilar(id) {
   return rp(url, options)
     .then(($) => {
       let similarNodes = $('#titleRecs .rec_page .rec_item[data-tconst]');
-      let ids = [];
-      similarNodes.each((i, el) => {
-        ids[i] = $(el).attr('data-tconst');
-      });
-
-      return Promise.map(ids, (simId) => {
-        return cachedMovie.getById(simId);
-      });
+      let ids = _.map(_.slice(similarNodes, 0, count), (el) => $(el).attr('data-tconst'));
+      return Promise.map(ids, (simId) => cachedMovie.getById(knex, simId));
     });
 }
 
