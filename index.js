a4836e276220a0f8d85a3518aa6ca8934a9010c5
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2017-05-12 13:06:41 +0200

    Add basic help message.

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ module.exports = function(bp) {
   const cachedMovie = require('./utils/cached_movie')(bp);
   const similar = require('./utils/similar_search')(bp);
 
+  bp.hear({platform: 'facebook', text: /help/}, (event, next) => {
+    bp.messenger.sendText(event.user.id, 'I\'m a very simple bot at the moment, so I may not understand everything you want.', {typing: 500})
+      .then(() => {
+        bp.messenger.sendText(event.user.id, 'You can type the name of a movie and I will try to find it for you.', {typing: 500});
+      })
+      .then(() => {
+        bp.messenger.sendText(event.user.id, 'Once I\'ve found a movie for you, you will be able to find out more about it by clicking buttons.', {typing: 500});
+        next();
+      });
+  });
+
   bp.hear({platform: 'facebook', type: 'message'}, (event) => {
     bp.messenger.sendText(event.user.id, 'Roger. Give me a second to look for "' + event.text + '".');
     imdb.get(event.text)
